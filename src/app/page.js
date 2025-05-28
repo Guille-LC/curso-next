3f77c3178ca3a052c985a40d57d637d69bb6ce7d
GU1LL3_L4-C0 <guillermo96lc@hotmail.com>
2025-05-27 21:12:52 -0300

    primera entrega

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,46 @@ import './styles/title.css'
 export default function Home() {
   return (
     <>
-      <h1 className="title text-5xl text-center font-extrabold dark:text-black">Catalina Posdeley</h1>
+      <h1 className="title">Catalina Posdeley</h1>
       <Carousel />
-      <p>
-        <strong className="block w-1/2 mx-auto text-center">
-          Quisque ac malesuada lacus. Mauris vel imperdiet elit, et semper velit. Nam aliquet ac mi quis laoree Quisque cursus semper dignissim. Maecenas nec accumsan nisl, a suscipit libero. Sed vel felis molli  egestas tellus vel, viverra est. Donec euismod lectus ac purus sagittis, sed faucibus felis facilisi  Suspendisse potenti. Nunc eget massa neque.
-        </strong>
-        <strong className="block w-1/2 mx-auto mt-4 text-center">
-          Donec id erat vel justo congue interdum. Donec interdum non sapien ac finibus. Suspendisse potenti. Don laoreet varius faucibus. Quisque efficitur felis sit amet euismod vehicula. Ut faucibus porttitor enim, s  scelerisque neque. Etiam iaculis, risus ac malesuada hendrerit, eros arcu pulvinar magna, pharet  venenatis tellus orci eget odio. Integer sed rhoncus dui. Donec ornare ex felis, non convallis dui sodal  id. Vestibulum interdum ex justo, consectetur vehicula felis facilisis sed.
-        </strong>
-      </p>
+      <div className="flexbooks">
+        {/* Libro 1 */}
+            <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
+                </a>
+                <div class="px-5 pb-5">
+                    <a href="#">
+                        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Jamas Podre Abrazarte</h5>
+                    </a>
+                    <div class="flex items-center mt-2.5 mb-5">
+                        <div class="flex items-center space-x-1 rtl:space-x-reverse"></div>
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <span class="text-3xl font-bold text-gray-900 dark:text-white">$8900</span>
+                        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Comprar</a>
+                    </div>
+                </div>
+            </div>
+            {/* Libro 2 */}
+            <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
+                </a>
+                <div class="px-5 pb-5">
+                    <a href="#">
+                        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Les Routes - La Teoria Del Caos</h5>
+                    </a>
+                    <div class="flex items-center mt-2.5 mb-5">
+                        <div class="flex items-center space-x-1 rtl:space-x-reverse"></div>
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <span class="text-3xl font-bold text-gray-900 dark:text-white">$19700</span>
+                        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Comprar</a>
+                    </div>
+                </div>
+            </div>
+      </div>
     </>
   );
 }
